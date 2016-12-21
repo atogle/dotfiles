df489ca754137f1f8872c8b6bdb32fb4a6b02790
Aaron Ogle <atogle@gmail.com>
2016-12-21 14:54:29 -0500

    Remove /usr/local/share/npm/bin from path.

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,6 @@ set -x EDITOR (which vim)
 set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/sbin $PATH
-set -x PATH /usr/local/share/npm/bin $PATH
 set -x PATH $HOME/.rbenv/bin $PATH
 set -x PATH $HOME/.rbenv/shims $PATH
 set -x PATH $HOME/bin $PATH
