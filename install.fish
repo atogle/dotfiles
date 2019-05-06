363377348ec1fd89a355422722b0a6f701e4b104
Aaron Ogle <atogle@gmail.com>
2019-05-06 17:59:06 -0400

    Add new install script using fish!

---
+++ install.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+set exclude_us Gemfile Gemfile.lock Thorfile README.md LICENSE.md fish
+
+echo Installing all dotfiles into $USER\'s home directory...
+for file in *
+    if not contains $file $exclude_us
+        echo $file
+        ln -f -s $PWD/$file ~/.$file
+    end
+end
+
+echo fish/config.fish
+ln -f -s $PWD/fish/config.fish ~/.config/fish/config.fish
+echo fish/functions
+ln -f -s $PWD/fish/functions/ ~/.config/fish/
