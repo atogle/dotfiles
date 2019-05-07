2c0d33bc3bee0d6bb73cb917f255e09eb774b87f
Aaron Ogle <atogle@gmail.com>
2019-05-07 15:46:54 -0400

    Remove more ruby dev stuff.

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,4 @@ set -x EDITOR (which vim)
 set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/sbin $PATH
-set -x PATH $HOME/.rbenv/bin $PATH
-set -x PATH $HOME/.rbenv/shims $PATH
 set -x PATH $HOME/bin $PATH
-set -x RUBYGEMS_EC2_DB1 ec2-54-245-133-190.us-west-2.compute.amazonaws.com
-set -x RUBYGEMS_EC2_LB1 54.245.255.174
--- fish/functions/irb.fish
+++
@@ -1,3 +0,0 @@
-function irb
-  pry
-end
