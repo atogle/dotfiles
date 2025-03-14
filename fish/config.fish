7c0c99a42a93076b4f053c7bf2818d8aefdcca50
Aaron Ogle <atogle@gmail.com>
2025-03-14 15:23:34 -0400

    Update config.fish to use Fish 4.0 features

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,34 @@
 set -g fish_greeting ''
 
-set -x EDITOR (which vim)
-set -x NODE_PATH /usr/local/lib/node_modules
-set -x PATH /usr/local/bin $PATH
-set -x PATH /usr/local/sbin $PATH
-set -x PATH $HOME/bin $PATH
+# Default editor
+set -x EDITOR (command -s vim)
 
-status --is-interactive; and source (rbenv init -|psub)
+# Node path
+set -x NODE_PATH /usr/local/lib/node_modules
+
+# PATH manipulation using Fish 4.0 path variable type
+fish_add_path /usr/local/bin
+fish_add_path /usr/local/sbin
+fish_add_path $HOME/bin
+
+# Check for Apple Silicon Homebrew
+if test -d /opt/homebrew
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/homebrew/sbin
+end
+
+# Git abbreviations (faster and more flexible than aliases)
+abbr -a ga 'git add'
+abbr -a gs 'git status'
+abbr -a gb 'git branch'
+abbr -a gca 'git commit -a'
+abbr -a gcm 'git commit -m'
+abbr -a gco 'git checkout'
+abbr -a gd 'git diff'
+abbr -a gl 'git log'
+abbr -a gp 'git push'
+abbr -a gpo 'git push origin'
+
+# Other useful abbreviations
+abbr -a ll '/bin/ls -AFGHhl'
+abbr -a brewup 'brew update; brew upgrade; brew cleanup'
