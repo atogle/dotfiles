768661747eb09902b92e72beaa91ef4db6a35cdb
Aaron Ogle <atogle@gmail.com>
2025-03-14 14:58:47 -0400

    Fix install.fish to link individual function files instead of the entire directory

--- install.fish
+++ install.fish
@@ -13,4 +13,12 @@ end
 echo fish/config.fish
 ln -f -s $PWD/fish/config.fish ~/.config/fish/config.fish
 echo fish/functions
-ln -f -s $PWD/fish/functions/ ~/.config/fish/
+# Create functions directory if it doesn't exist
+mkdir -p ~/.config/fish/functions
+
+# Link individual function files instead of the entire directory
+for func in fish/functions/*.fish
+    set func_name (basename $func)
+    echo "  $func_name"
+    ln -f -s $PWD/$func ~/.config/fish/functions/$func_name
+end
