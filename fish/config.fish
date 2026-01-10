86ca3cdf92a10e3ea0193fb3dbce3a2e5f496d03
Aaron Ogle <atogle@gmail.com>
2026-01-09 22:26:25 -0500

    Modernize dotfiles configuration
    - install.fish: Fix path resolution and add exclusions for .git/.DS_Store
    - gitconfig: Update push default to 'simple' and fix status alias
    - config.fish: Use modern 'abbr --add', 'fish_add_path', and interactive check

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-set -g fish_greeting ''
-
 # Default editor
 set -x EDITOR (command -s vim)
 
@@ -17,23 +15,27 @@ if test -d /opt/homebrew
     fish_add_path /opt/homebrew/sbin
 end
 
-# Git abbreviations (faster and more flexible than aliases)
-abbr -a ga 'git add'
-abbr -a gs 'git status'
-abbr -a gb 'git branch'
-abbr -a gca 'git commit -a'
-abbr -a gcm 'git commit -m'
-abbr -a gco 'git checkout'
-abbr -a gd 'git diff'
-abbr -a gl 'git log'
-abbr -a gp 'git push'
-abbr -a gpo 'git push origin'
+if status is-interactive
+    set -g fish_greeting ''
 
-# Other useful abbreviations
-abbr -a ll '/bin/ls -AFGHhl'
-abbr -a brewup 'brew update; brew upgrade; brew cleanup'
-abbr -a p 'cd ~/Projects'
-abbr -a ls '/bin/ls -FGH'
-abbr -a vi 'vim'
-abbr -a mkdir 'command mkdir -p'
-abbr -a clean 'find . -name'
+    # Git abbreviations (faster and more flexible than aliases)
+    abbr --add ga 'git add'
+    abbr --add gs 'git status'
+    abbr --add gb 'git branch'
+    abbr --add gca 'git commit -a'
+    abbr --add gcm 'git commit -m'
+    abbr --add gco 'git checkout'
+    abbr --add gd 'git diff'
+    abbr --add gl 'git log'
+    abbr --add gp 'git push'
+    abbr --add gpo 'git push origin'
+
+    # Other useful abbreviations
+    abbr --add ll '/bin/ls -AFGHhl'
+    abbr --add brewup 'brew update; brew upgrade; brew cleanup'
+    abbr --add p 'cd ~/Projects'
+    abbr --add ls '/bin/ls -FGH'
+    abbr --add vi 'vim'
+    abbr --add mkdir 'command mkdir -p'
+    abbr --add clean 'find . -name'
+end
--- install.fish
+++ install.fish
@@ -1,24 +1,28 @@
 #!/usr/bin/env fish
 
-set exclude_us README.md LICENSE.md install.fish fish
+# Ensure we are in the directory of the script to get absolute paths correct
+cd (dirname (status -f))
+set DOTFILES (pwd)
+set exclude_us README.md LICENSE.md install.fish fish .git .DS_Store
 
 echo Installing all dotfiles into $USER\'s home directory...
-for file in *
-    if not contains $file $exclude_us
-        echo $file
-        ln -f -s $PWD/$file ~/.$file
+for file in $DOTFILES/*
+    set filename (basename $file)
+    if not contains $filename $exclude_us
+        echo $filename
+        ln -f -s $file ~/.$filename
     end
 end
 
 echo fish/config.fish
-ln -f -s $PWD/fish/config.fish ~/.config/fish/config.fish
+ln -f -s $DOTFILES/fish/config.fish ~/.config/fish/config.fish
 echo fish/functions
 # Create functions directory if it doesn't exist
 mkdir -p ~/.config/fish/functions
 
 # Link individual function files instead of the entire directory
-for func in fish/functions/*.fish
+for func in $DOTFILES/fish/functions/*.fish
     set func_name (basename $func)
     echo "  $func_name"
-    ln -f -s $PWD/$func ~/.config/fish/functions/$func_name
+    ln -f -s $func ~/.config/fish/functions/$func_name
 end
